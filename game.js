da4012d79ba9977461de305b0a5ccc33816e81b9
graholm <graholm@nuevaschool.org>
2016-09-27 09:31:13 -0700

    Classadoo autocommit

--- game.js
+++ game.js
@@ -4,13 +4,17 @@ console.log ("quantityInput");
 quantityInput.keydown(function(e) {
 
   var keyCode = e.keyCode
-  console.log (e.keyCode)
   var inputValue = quantityInput.val();
  
-  if (keyCode == 72) {	
+  if (keyCode == 13) {	
     console.log ("H");
+  } 
+  if (keyCode == 81) {	
+    console.log ("");
   }
-  
+  if (keyCode == 13) {	
+    console.log (inputValue);
+  }
   if (keyCode == 13) {	
     console.log (inputValue);
   }
