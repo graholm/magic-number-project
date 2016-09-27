5f455c99947505dcb5e853f2cc1d473b41957c0b
graholm <graholm@nuevaschool.org>
2016-09-27 09:24:50 -0700

    Classadoo autocommit

--- game.js
+++ game.js
@@ -6,15 +6,11 @@ quantityInput.keydown(function(e) {
   var keyCode = e.keyCode
   console.log (e.keyCode)
   var inputValue = quantityInput.val();
-  if (hour < 18) {
-    greeting = "Good day";
-  } else {
-    greeting = "Good evening";
-  }
+ 
   if (keyCode == 13) {    
    console.log (inputValue);
   }
- else (keyCode == 72) {	    
+  
+  if (keyCode == 72) {	    
     console.log ("yay you wrote h");
   }
-})
