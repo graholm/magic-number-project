f99c680bcbce024ecf972840079bab04ca0dbd8d
graholm <graholm@nuevaschool.org>
2016-09-27 09:13:36 -0700

    Classadoo autocommit

--- game.js
+++ game.js
@@ -1,18 +1,3 @@
-var obj = {
-  a: 1,
-  b: 2,
-  c: 3,
-  d: 4,
-  e: 5,
-  f: 6,
-}
-
-
-
-var text = 'hello';
 text.split('').forEach(function(c) {
   console.log(c);
 });
-
-
-console.log(obj["a"] + obj["d"])
