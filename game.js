90756ed9dda09c2623e0757ba30c2ca768ef09dd
graholm <graholm@nuevaschool.org>
2016-09-27 09:16:45 -0700

    Classadoo autocommit

--- game.js
+++ game.js
@@ -2,6 +2,7 @@ var quantityInput = $(".element-quantity-input")
 console.log ("quantityInput");
 
 quantityInput.keydown(function(e) {
+  console.log e.keyCode
   var keyCode = e.keyCode
   var inputValue = quantityInput.val();
 
