b21b8d1e21ef1279cdd0f7c5cefa9d76932de670
graholm <graholm@nuevaschool.org>
2016-10-05 09:09:00 -0700

    Classadoo autocommit

--- game.js
+++ game.js
@@ -20,7 +20,7 @@ $.each(arrValues, function (intIndex, char) {
 console.log (sum);
     
     sum = Math.round(sum /40)
-      <button type="button" onclick="document.write(sum)">Try it</button>
+    
     console.log (sum);
   }
 })
