81da12bc8d0cd6b806f5612dfd9f6a8647446717
graholm <graholm@nuevaschool.org>
2016-12-14 09:12:59 -0800

    Classadoo autocommit

---
+++ game.js
@@ -0,0 +1,28 @@
+var valueseason = $("#dropDownId").val();
+
+var quantityInput = $(".element-quantity-input")
+quantityInput.keydown(function(e) {
+
+  var keyCode = e.keyCode
+  var inputValue = quantityInput.val();
+
+  if (keyCode == 13) {
+    var valueseason = $("#dropDownId").val();
+
+    var season = (valueseason); 
+    console.log (season)
+  
+var arrValues = inputValue.split('');
+var sum = 0;     
+$.each(arrValues, function (intIndex, char) {
+ sum = sum + char.charCodeAt(0); 
+})
+console.log (sum)
+    
+sum = Math.round (sum /season)
+    
+    console.log (sum);
+    
+    document.write(sum)
+  }
+})
